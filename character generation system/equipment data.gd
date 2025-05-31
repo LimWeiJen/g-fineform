41c0d0326d64074c19b7f53554c30ed4d66b29ff
LimWeiJen <limweijen96@gmail.com>
2025-05-31 08:41:36 +0800

    1

---
+++ character generation system/equipment data.gd
@@ -0,0 +1,5 @@
+# equipment_data.gd
+const MAIN_HAND = ["Sword", "Staff", "Dagger", "Axe"]
+const ARMOR = ["Leather", "Robe", "Plate", "Cloth"]
+const ACCESSORIES = ["Ring of Power", "Necklace of Shadows", "None"]
+
---
+++ character generation system/trait data.gd
@@ -0,0 +1,6 @@
+const TRAITS = {
+    "Brave": {"Martial": 2, "Combat": 1},
+    "Cunning": {"Intrigue": 3},
+    "Wise": {"Learning": 2},
+    "Charming": {"Diplomacy": 2}
+}
